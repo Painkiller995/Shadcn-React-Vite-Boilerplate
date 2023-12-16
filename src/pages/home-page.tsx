7d5dcb78e8da12996f06cc16d6ceb5587b2510ee
fahdd95 <fahdakramdaher@gmail.com>
2023-12-16 01:15:35 +0100

    change style

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -11,13 +11,15 @@ export default function HomePage() {
         <title>Welcome to Our Website</title>
       </Helmet>
 
-      <section className="flex h-screen flex-col items-center justify-center">
+      <section className="container flex  h-screen flex-col items-center justify-center gap-6 pb-8 pt-10 md:py-10">
         <h1 className="text-3xl font-extrabold leading-tight tracking-tighter md:text-4xl">
           Welcome to Our Website
         </h1>
+
         <p className="max-w-[700px] pt-4 text-lg text-muted-foreground">
           Explore our amazing features and discover what makes us unique.
         </p>
+
         <p className="mb-4 pt-5 text-2xl">🚀🌐</p>
         <Link to={sitePaths.about}>
           <div
