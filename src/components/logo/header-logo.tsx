9549182a9fe099c3cbe6f4bc59ccfb71bd434129
fahdd95 <fahdakramdaher@gmail.com>
2023-12-17 23:39:24 +0100

    update padding

--- src/components/logo/header-logo.tsx
+++ src/components/logo/header-logo.tsx
@@ -5,7 +5,7 @@ import { sitePaths } from '@/configurations/paths';
 import { Icons } from '../icons';
 
 export const HeaderLogo = () => (
-  <Link className=" hidden gap-5 px-4 lg:flex lg:items-center" to={sitePaths.home}>
+  <Link className=" hidden gap-5 pr-8 lg:flex lg:items-center" to={sitePaths.home}>
     <Icons.Logo className="h-6 w-6" />
   </Link>
 );
