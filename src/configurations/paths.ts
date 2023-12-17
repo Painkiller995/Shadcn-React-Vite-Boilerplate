f61c4f167c57a2c60fba7355dd9b835872496f75
fahdd95 <fahdakramdaher@gmail.com>
2023-12-17 19:52:48 +0100

    add description

--- src/configurations/paths.ts
+++ src/configurations/paths.ts
@@ -13,35 +13,43 @@ export const sitePaths = {
 export const pathsArr: NavItem[] = [
   {
     title: 'Home',
+    description: 'Navigate to the home page',
     href: sitePaths.home,
   },
   {
     title: 'Page One',
+    description: 'Go to the first page',
     href: sitePaths.pageOne,
   },
   {
     title: 'About',
+    description: 'Learn more about us',
     href: sitePaths.about,
   },
   {
     title: '403',
+    description: 'Access forbidden',
     href: sitePaths.page403,
   },
   {
     title: '404',
+    description: 'Page not found',
     href: sitePaths.page404,
   },
   {
     title: '500',
+    description: 'Internal server error',
     href: sitePaths.page500,
   },
   {
     title: 'Google New Tab',
+    description: 'Open Google in a new tab (external link)',
     href: 'https://www.google.com/',
     external: true,
   },
   {
     title: 'Disabled',
+    description: 'This link is disabled',
     href: '/disabled',
     disabled: true,
   },
